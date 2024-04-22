e8560a31ff202b40bc68f12a731904fb6195d94d
Shiraz <shirazkk1@gmail.com>
2024-04-22 17:16:55 +0530

    Added controller and routes for create tweet api

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import express from "express";
-import { connect } from "./config/database.js";
-const app = express();
+import bodyParser from "body-parser";
 
-import service from "./services/tweet-services.js";
+import { connect } from "./config/database.js";
+
+import apiRoutes from "./routes/index.js";
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use("/api", apiRoutes);
 
 app.listen(3000, async () => {
-  console.log("Server started");
+  console.log("server started");
   await connect();
   console.log("Mongo db connected");
-  let ser = new service();
-  await ser.create({ content: "capital #CoDE ?" });
 });
--- src/services/tweet-services.js
+++ src/services/tweet-services.js
@@ -9,9 +9,7 @@ class TweetService {
     const content = data.content;
     const tags = content
       .match(/#[a-zA-Z0-9_]+/g)
-      .map((tag) => tag.substring(1))
-      .map((tag) => tag.toLowerCase()); // this regex extract hashtags
-    console.log("tags-> ", tags);
+      .map((tag) => tag.substring(1).toLowerCase()) // this regex extract hashtags
     const tweet = await this.tweetRepository.create(data);
     let alreadyPresentTags = await this.hashtagRepository.findByName(tags);
     let titleOfPresentTags = alreadyPresentTags.map((tags) => tags.title);
