90a40b4dade87f2683470154fdbb519ec1128c5e
Shiraz <shirazkk1@gmail.com>
2024-04-22 16:25:01 +0530

    Moved to es modules

--- src/services/tweet-services.js
+++ src/services/tweet-services.js
@@ -1,4 +1,4 @@
-const { TweetRepository, HashtagRepository } = require("../repository/index");
+import { TweetRepository, HashtagRepository } from "../repository/index.js";
 
 class TweetService {
   constructor() {
@@ -24,4 +24,5 @@ class TweetService {
     return tweet;
   }
 }
-module.exports = TweetService;
+ 
+export default TweetService;
