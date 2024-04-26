3a42b9486d0cb63ed09eb027c4161d2e15828005
Shiraz <shirazkk1@gmail.com>
2024-04-26 15:43:58 +0530

    Added basic crud repo and extended itin likerepo

--- src/repository/tweet-repository.js
+++ src/repository/tweet-repository.js
@@ -1,18 +1,13 @@
 import Tweet from "../models/tweet.js";
+import CrudRepository from "./crud-repository.js";
+class TweetRepository extends CrudRepository {
+  constructor() {
+    super(Tweet);
+  }
 
-class TweetRepository {
   async create(data) {
     try {
       const tweet = await Tweet.create(data);
-      return tweet;
-    } catch (error) {
-      console.log(error);
-    }
-  }
-
-  async get(id) {
-    try {
-      const tweet = await Tweet.findById(id);
       return tweet;
     } catch (error) {
       console.log(error);
@@ -30,14 +25,6 @@ class TweetRepository {
     }
   }
 
-  async destroy(id) {
-    try {
-      const tweet = await Tweet.findByIdAndRemove(id);
-      return tweet;
-    } catch (error) {
-      console.log(error);
-    }
-  }
   async getAll(offset, limit) {
     try {
       const tweet = await Tweet.find().skip(offset).limit(limit);
