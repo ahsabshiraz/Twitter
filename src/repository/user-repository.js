7a2a71cd00f41433ae18842f41763371eb641ffe
Shiraz <shirazkk1@gmail.com>
2024-04-28 15:50:47 +0530

    Added passport logc

--- src/repository/user-repository.js
+++ src/repository/user-repository.js
@@ -5,6 +5,14 @@ class UserRespository extends CrudRepository {
   constructor() {
     super(User);
   }
+  async findBy(data) {
+    try {
+      const response = await User.findOne(data);
+      return response;
+    } catch (error) {
+      throw error;
+    }
+  }
 }
 
 export default UserRespository;
